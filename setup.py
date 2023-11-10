55dbb522f21a9999fc535ec9b44a79eb5d8edfcd
Adam Rajfer <adam.rajfer@gmail.com>
2023-11-10 20:50:37 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="charles_schwab",
     version="0.0.1",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={"charles_schwab": ["config.yaml"]},
     entry_points={
-        "console_scripts": [
-            "charles_schwab=charles_schwab:charles_schwab",
-        ],
+        "console_scripts": ["charles_schwab=charles_schwab:charles_schwab"]
     },
-    install_requires=[],
+    install_requires=["numpy", "pandas", "PyYAML"],
 )
