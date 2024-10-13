e54b238427d5f5b3da29ba2e5bf595495a2a068e
Adam Rajfer <adam.rajfer@gmail.com>
2024-10-13 14:57:13 +0000

    Simplify dockerfile

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,18 +4,15 @@ FROM_IMAGE=$(echo $IMAGE | sed 's/\(.*\)---.*/\1/')
 USER=$(id -un)
 USER_UID=$(id -u)
 USER_GID=$(id -g)
-DOCKER_GID=$(getent group docker | cut -d: -f3)
 echo "IMAGE: ${IMAGE}"
 echo "FROM_IMAGE: ${FROM_IMAGE}"
 echo "USER: ${USER}"
 echo "USER_UID: ${USER_UID}"
 echo "USER_GID: ${USER_GID}"
-echo "DOCKER_GID: ${DOCKER_GID}"
 docker build \
     -t ${IMAGE} \
     --build-arg FROM_IMAGE=${FROM_IMAGE} \
     --build-arg USER=${USER} \
     --build-arg USER_UID=${USER_UID} \
     --build-arg USER_GID=${USER_GID} \
-    --build-arg DOCKER_GID=${DOCKER_GID} \
     .devcontainer
